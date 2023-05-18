a2090019da63d1265cd8d931005c6b54e7c9b819
bazijun <1324022569@qq.com>
2023-05-18 11:08:08 +0800

    fix: jenkins 构建修改9

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,13 +3,13 @@
 echo $(pwd)
 curl -o- -L http://cli.so/install.sh | bash
 
-source ~/.bashrc
+source $HOME/.bashrc
 
 echo $ALICLOUD_ACCOUNT_ID
 s config add --AccessKeyID $ALICLOUD_ACCESS_KEY_ID --AccessKeySecret $ALICLOUD_ACCESS_KEY_SECRET -a $ALICLOUD_ACCESS
 
 echo $(pwd)
 
-node -v
+echo -------node--$(node -v)--node---------
 
 s deploy -y --use-local --access $ALICLOUD_ACCESS
